2f8bdee22af1d8e9b4b66ec3736e4e1383169c39
Andre <andre.bulatov@gmail.com>
2015-01-13 00:39:19 -0500

    Update CategoryPageColorSwatchJS.js

--- CategoryPageColorSwatchJS.js
+++ CategoryPageColorSwatchJS.js
@@ -1,26 +1,50 @@
-/* 	Existance checker function */
-/*
-	$.fn.exists = function(callback) {
-		var args = [].slice.call(arguments, 1);
-		if (this.length) {
-			callback.call(this, args);
-		}
-		return this;
-	};
-*/
+// Color Swatch and Size Boxes
+	$(".Options").each(function checkForVideo(url) {
+		var ProductCatOpt = $(this);
+			ProductId = $(this).parent().parent().find('div.ProductImage').attr('data-product');
+		function ajax1() {
+		    return $.ajax('/content/Videos/'+ProductId+'.mp4')
+			    .done(function() { 
+			        ProductCatOpt.addClass('withVideo');
+			    }).fail(function() { 
+			    	return;
+			    });
+			}
+			$.when(ajax1()).done(function(a1){
+		        ProductCatOpt.closest('li').append('<span class="videoDemoBtn"><div class="triangle"></div></span>');
+	        });
+	    });
 
-/* 	Using ECF to check for Video Option Trigger, hiding it, and adding buttons */
+
+	$('.ProductList').on('click', '.videoDemoBtn', function () {
+	        if ($(this).hasClass('videoPlaying')) {
+	            $(this).removeClass('videoPlaying');
+/* 	            $(this).parent().find('img').show(); */
+	            $(this).parent().find('div.categoryDemoVideo').hide().html('');
+	            }
+	            else {
+	                var ProductId = $(this).parent().find('div.ProductImage').attr('data-product');
+	                $(this).addClass('videoPlaying');
+/* 	                $(this).parent().find('img').hide();         */
+	                $(this).parent().find('div.categoryDemoVideo').show().html(
+	                	'<video id="demoVideo" class="video" preload autoplay loop autobuffer controls muted width="100%" height="100%" poster="/content/Videos/'+ProductId+'.jpg">'+
+	                		'<source src="/content/Videos/'+ProductId+'.mp4">'+
+	                		'<source src="/content/Videos/'+ProductId+'.ogv" type="video/ogg">'+
+	                		'<p>Your browser does not support this video.  Please upgrade your browser!</p>'+
+	                	'</video>');
+	                        }
 /*
-	$(function () {
-		$("span:contains('HasDemoVideo')").exists(function() {
-			$(this).closest('.productAttributeRow').hide();
-			var ProductThumb = $('.ProductThumb');
-				ProductTinyImageList = $('.ProductTinyImageList ul');
-			ProductThumb.append('<span id="videoDemoBtn" class=""><div class="triangle"></div></span>');
-			ProductTinyImageList.append('<li id="videoDemoThumb" class="" style="height:%%GLOBAL_ProductTinyBoxHeight%%px; width:%%GLOBAL_ProductTinyBoxWidth%%px;"><div class="ProdVideoPlayBtn"><div class="triangle"></div></div></li>');
-			});    
-		});
+                        	var video = document.getElementById('demoVideo');
+							video.addEventListener('click',function(){
+							  video.play();
+							},false);
 */
+	                });
+
+
+
+
+
 
 
 
