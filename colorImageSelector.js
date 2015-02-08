59923f45733f82c13adfaf45515c0533563434ac
Andre Bulatov <andre.bulatov@gmail.com>
2015-02-08 18:36:28 -0500

    for loop

--- colorImageSelector.js
+++ colorImageSelector.js
@@ -12,20 +12,22 @@ on click
 $("span:contains('Color')").parents().eq(2).find('.productAttributeValue li').each( function getColorPhotosSrcs(){
 //console.log(this); 
 	// add .selectedValue class to li 
-	$(this).addClass('selectedValue'); 
+	$(this).find('label').click();
 	// add .checked class to li 
-	$(this).find('.radio span').addClass('checked'); 
+	//$(this).find('.radio span').addClass('checked'); 
 //console.log(this);
 	// find url of img when this li is checked and store it
-	var colorImgSrc = $(document).find('.MagicZoomPlus img').attr('src'); 
-console.log(colorImgSrc); 
+	for (i = 0; i < this.length; i++) {
+		var colorImgSrc[] = $(document).find('.MagicZoomPlus img').attr('src'); 
+		console.log(colorImgSrc); 
+	}
 //	console.log(this);
-	$(this).removeClass('selectedValue'); 
-	$(this).find('.radio span').removeClass('checked'); 
+	//$(this).removeClass('selectedValue'); 
+	//$(this).find('.radio span').removeClass('checked'); 
 })
 
 
 $("span:contains('Color')").parents().eq(2).find('.productAttributeValue li').removeClass('selectedValue').find('.radio span').removeClass('checked'); 
 $("span:contains('Color')").parents().eq(2).find('.productAttributeValue li').eq(2).addClass('selectedValue').find('.radio span').addClass('checked'); 
 
-$("span:contains('Color')").parents().eq(2).find('.productAttributeValue li').eq(2).find().click();
+$("span:contains('Color')").parents().eq(2).find('.productAttributeValue li').eq(2).find('label').click();
