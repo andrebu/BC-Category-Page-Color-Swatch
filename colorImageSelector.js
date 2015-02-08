35a40e96acac3940fe3e7a289160185f7aa776e6
Andre Bulatov <andre.bulatov@gmail.com>
2015-02-08 18:35:45 -0500

    Update

---
+++ colorImageSelector.js
@@ -0,0 +1,27 @@
+/*
+for each li 
+	color = find span color --- this.text
+	url = find main prod image -- this.url
+	category Swatch Color Button = matching color.url
+end
+
+on click 
+*/
+	
+	
+$("span:contains('Color')").parents().eq(2).find('.productAttributeValue li').each( function getColorPhotosSrcs(){
+//console.log(this); 
+	// add .selectedValue class to li 
+	$(this).addClass('selectedValue'); 
+	// add .checked class to li 
+	$(this).find('.radio span').addClass('checked'); 
+//console.log(this);
+	// find url of img when this li is checked and store it
+	var colorImgSrc = $(document).find('.MagicZoomPlus img').attr('src'); 
+console.log(colorImgSrc); 
+//	console.log(this);
+	$(this).removeClass('selectedValue'); 
+	$(this).find('.radio span').removeClass('checked'); 
+})
+
+
