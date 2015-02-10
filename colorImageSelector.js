d5138c3c55d4dddcf519ac2dea1704cd7b0f2695
Andre Bulatov <andre.bulatov@gmail.com>
2015-02-09 20:55:05 -0500

    Getting closer

--- colorImageSelector.js
+++ colorImageSelector.js
@@ -1,41 +1,3 @@
-/*
-for each li 
-	color = find span color --- this.text
-	url = find main prod image -- this.url
-	category Swatch Color Button = matching color.url
-end
-
-on click 
-*/
-	
-
-var colorBtnSrc = {};
-	
-$("span:contains('Color')").parents().eq(2).find('.productAttributeValue li').each( function (){
-
-	var listings = $(this);
-		prodColor = $(this).find('.name').text();
-	console.log(prodColor);
-
-	$.when(
-		$(this).find('label').click()
-	)
-	.then( function() {
-		colorBtnSrc[prodColor] = $(this).parents().find('.ProductThumbImage a').attr('href');
-console.log(colorBtnSrc[prodColor])		
-	});
-console.log(colorBtnSrc[prodColor])
-})
-
-//$("span:contains('Color')").parents().eq(2).find('.productAttributeValue li').removeClass('selectedValue').find('.radio span').removeClass('checked'); 
-//$("span:contains('Color')").parents().eq(2).find('.productAttributeValue li').eq(2).addClass('selectedValue').find('.radio span').addClass('checked'); 
-
-$("span:contains('Color')").parents().eq(2).find('.productAttributeValue li').eq(2).find('label').click();
-
-
-
-
-
 	$(".Options").each(function checkForOptionSwatches(url) {
 	    var productListing = $(this).closest('em.p-price').html();
 	    productLink = $(this).parent().parent().find('div.ProductImage a').attr('href');
@@ -55,28 +17,26 @@ $("span:contains('Color')").parents().eq(2).find('.productAttributeValue li').eq
 	            result = data;
 	            var productPage = result;
 	            $productPageProcessing = $(productPage).find("span:contains('Color')").parents().eq(2).find('.productAttributeValue li');
+	            colorBtnSrc = {};
 
-	            var colorBtnSrc = {};
-
-	            $productPageProcessing.each(function() {
+	            var FunctionOne = $productPageProcessing.each(function() {
 
 	                var listings = $(this);
 	                prodColor = $(this).find('.name').text();
 	                console.log(prodColor);
 
-	                $.when(
-	                        $(this).find('label').click()
-	                    )
-	                    .then(function() {
-	                        colorBtnSrc[prodColor] = $(this).parents().find('.ProductThumbImage a').attr('href');
-	                        console.log(colorBtnSrc[prodColor])
-	                    });
-	                console.log(colorBtnSrc[prodColor])
-	            })
+	                $(this).find('label').click();
+	            });
+
+	            var FunctionTwo = function() {
+	                colorBtnSrc[prodColor] = $(this).parents().find('.ProductThumbImage a').attr('href');
+	                console.log(colorBtnSrc[prodColor]);
+	            };
+	            console.log(colorBtnSrc[prodColor])
+
+	            FunctionOne().done(FunctionTwo);
 
 	        }).fail(function() {
 	            return;
 	        });
 	});
-
-
