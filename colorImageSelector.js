9920aa37ffe36d10413f72f4f639fbdfec2da21e
Andre Bulatov <andre.bulatov@gmail.com>
2015-02-09 20:19:05 -0500

    Building into ajax

--- colorImageSelector.js
+++ colorImageSelector.js
@@ -35,3 +35,49 @@ $("span:contains('Color')").parents().eq(2).find('.productAttributeValue li').eq
 
 
 
+
+	$(".Options").each(function checkForOptionSwatches(url) {
+		var productListing = $(this).closest('em.p-price').html() ;
+			productLink = $(this).parent().parent().find('div.ProductImage a').attr('href');
+		var	$this = $(this);
+		function getOptionSwatches() {
+			return $.ajax({
+		            url: productLink,
+		            type: 'GET',
+		            dataType: 'html',
+		            //async: false
+		    });
+		}
+		getOptionSwatches()
+			.done(function(data) { 
+		
+	                result = data;
+    				var productPage = result;
+						$productPageProcessing = $( productPage ).find("span:contains('Color')").parents().eq(2).find('.productAttributeValue li');	
+
+				var colorBtnSrc = {};
+					
+				$productPageProcessing.each( function (){
+				
+					var listings = $(this);
+						prodColor = $(this).find('.name').text();
+					console.log(prodColor);
+				
+					$.when(
+						$(this).find('label').click()
+					)
+					.then( function() {
+						colorBtnSrc[prodColor] = $(this).parents().find('.ProductThumbImage a').attr('href');
+				console.log(colorBtnSrc[prodColor])		
+					});
+				console.log(colorBtnSrc[prodColor])
+				})
+
+			    }).fail(function() { 
+			    	return;
+			    });
+			}
+	    );
+
+
+
