f567694d6905e2d2ed2d225b490ec4fcb61bd0e8
Andre Bulatov <andre.bulatov@gmail.com>
2015-02-27 16:35:51 -0500

    Cleanup update

--- CategoryPageColorSwatchJS.js
+++ CategoryPageColorSwatchJS.js
@@ -1,15 +1,21 @@
     $('<style type="text/css">\
 			/* Color and Size Options on Category Pages */\
-			.catSizeSwatch {\
+			/*.catSizeSwatch {\
 				display: none;\
-			}\
+			}*/\
 			.catColorSwatch {\
 				display: inline-block;\
 			}\
-			.catColorSwatch .validation,\
-			.catColorSwatch .name,\
-			.catSizeSwatch .validation,\
-			.catSizeSwatch .name {\
+			.catColorSwatch li {\
+				padding: 0 4px;\
+			}\
+			.catColorSwatch .swatchOneColour .validation,\
+			.catColorSwatch .swatchOneColour .name,\
+			.catSizeSwatch .validation/*,\
+			.catSizeSwatch .name*/ {\
+				display: none;\
+			}\
+			.catColorSwatch li .validation {\
 				display: none;\
 			}\
 			.catColorSwatch .swatchColour {\
@@ -18,6 +24,7 @@
 				height: 17px !important;\
 				border: 1px solid #fff;\
 			}\
+			.catColorSwatch li,\
 			.catColorSwatch li.swatch.swatchOneColour {\
 				display: inline-block;\
 				float: left;\
@@ -29,21 +36,100 @@
 			.catColorSwatch li.swatch.swatchOneColour.selectedColorBox {\
 				padding: 0;\
 			}\
-			.catColorSwatch .swatch.swatchOneColour label {\
+			.catColorSwatch li label {\
 				margin: 0;\
 				line-height: normal;\
 			}\
-			.catColorSwatch .swatch.swatchOneColour span.swatchColours.swatchOneColour {\
+			.catColorSwatch li.swatch.swatchOneColour label span.swatchColours.swatchOneColour {\
 				line-height: inherit;\
+			}\
+			.catColorSwatch .name {\
+				line-height: 19px;\
+				padding: 0 4px;\
+			}\
+			.catSizeSwatch {\
+				background-size: auto 100%;\
+				position: absolute;\
+				top: 253px;\
+				right: 0px;\
+				padding: 3px 0;\
+				width: 45px;\
+			}\
+			.catSizeSwatch .list-horizontal {\
+				margin: 0;\
+			}\
+			.catSizeSwatch .list-horizontal .option {\
+				display: inline-block;\
+				width: 45px;\
+				padding: 0;\
+				background: none;\
+			}\
+			.catSizeSwatch .list-horizontal .option label {\
+				margin: 0;\
+			}\
+			.catSizeSwatch .list-horizontal .option label .name {\
+			    font-weight: bold;\
+			    color: black;\
+			    display: block;\
+			    font-size: 8px;\
+			}\
+			.sizeOptionsOverlay {\
+				/* background: url("%%ASSET_images/JointSizesL.png%%") center center no-repeat; */\
+				background-size: auto 100%;\
+				position: absolute;\
+				top: 253px;\
+				right: 0px;\
+				padding: 3px 0;\
+				width: 45px;\
+			}\
+			.catSizeSwatch .jointTitle,\
+			.sizeOptionsOverlay .jointTitle {\
+			    font-weight: bold;\
+				display: inline-block;\
+			    line-height: 9px;\
+			    font-size: 8px;\
+			}\
+			.sizeOptionsOverlay .jointSize {\
+			    display: inline-block;\
+			    text-align: center;\
+			    color: #BBB;\
+			    vertical-align: bottom;\
+			}\
+			.catSizeSwatch img,\
+			.sizeOptionsOverlay .jointSize img {\
+			    width: 14px;\
+			    display: block;\
+			    margin: 0 auto;\
+			}\
+			.sizeOptionsOverlay .jointSize span {\
+			    font-weight: bold;\
+			    color: black;\
+			    display: block;\
+			    font-size: 8px;\
+			}\
+			.sizeOptionsOverlay .jointSize i {\
+			    display: block;\
+			}\
+			.catSizeSwatch img.small,\
+			.sizeOptionsOverlay .jointSize.small img {\
+			    width: 10px;\
+			}\
+			.catSizeSwatch img.big,\
+			.sizeOptionsOverlay .jointSize.big img {\
+			    width: 18px;\
 			}\
 			</style>')
         .appendTo('head');
+    jQuery.expr[':'].icontains = function(a, i, m) {
+	  return jQuery(a).text().toUpperCase()
+	      .indexOf(m[3].toUpperCase()) >= 0;
+	};   
     /* try to combine this with category videos! */
     $(document).ready(function() {
         // Swatches - Color and Size options swatches 
         $(".Options").each(function checkForOptionSwatches(url) {
             var productListing = $(this).closest('em.p-price').html();
-            productLink = $(this).parent().parent().find('div.ProductImage a').attr('href');
+            var productLink = $(this).parent().parent().find('div.ProductImage a').attr('href');
             var $this = $(this);
 
             function getOptionSwatches() {
@@ -59,9 +145,12 @@
 
                     result = data;
                     var productPage = result;
-                    $productPageProcessing = $(productPage).find('.productAttributeList');
-                    colorSwatch = $productPageProcessing.find("span:contains('Color')").parents().eq(2).find('.productAttributeValue div').html();
-                    sizeSwatch = $productPageProcessing.find("span:contains('Size')").parents().eq(2).find('.productAttributeValue div').html();
+                    var $productPageProcessing = $(productPage).find('.productAttributeList');
+                    var jointSizeAndGender = $(productPage).find('.JointSizeHeightContainer .Label:icontains("Joint Type:")').parent().find('.Value').text();
+                    var maleOrFemale = $.trim(jointSizeAndGender).split(' ')[1];
+                    var colorSwatch = $productPageProcessing.find("span:icontains('Color')").parents().eq(2).find('.productAttributeValue div').html();
+                    var sizeSwatch = $productPageProcessing.find("span:icontains('Size')").parents().eq(2).find('.productAttributeValue div').html();
+                    var sizeOptionsOverlay = '<div class="catSizeSwatch optionSwatch"><span class="jointTitle">Fits these joint sizes:</span>' + sizeSwatch + '</div>';
                     if (colorSwatch != null) {
                         $this.parent().parent().find('.ProductDetails').before('<div class="catColorSwatch optionSwatch">' + colorSwatch + '</div>');
                         $this.addClass('withColorSwatch');
@@ -162,7 +251,19 @@
                         // Color swatch clickability ends here
                     };
                     if (sizeSwatch != null) {
-                        $this.parent().parent().find('em.p-price').before('<div class="catSizeSwatch optionSwatch">' + sizeSwatch + '</div>');
+	                    var thisProductListing = $this.parent().parent();
+	                    if (maleOrFemale == 'Female') {
+	                        thisProductListing.find('.ProductDetails').before(sizeOptionsOverlay);
+	                        thisProductListing.find('.name:icontains("10mm")').after('<img class="small" src="%%ASSET_images/malejoint.png%%" />');
+	                        thisProductListing.find('.name:icontains("14mm")').after('<img src="%%ASSET_images/malejoint.png%%" />');
+	                        thisProductListing.find('.name:icontains("18mm")').after('<img class="big" src="%%ASSET_images/malejoint.png%%" />');
+	                    };
+	                    if (maleOrFemale == 'Male') {
+	                        thisProductListing.find('.ProductDetails').before(sizeOptionsOverlay);
+	                        thisProductListing.find('.name:icontains("10mm")').after('<img class="small" src="%%ASSET_images/femalejoint.png%%" />');
+	                        thisProductListing.find('.name:icontains("14mm")').after('<img src="%%ASSET_images/femalejoint.png%%" />');
+	                        thisProductListing.find('.name:icontains("18mm")').after('<img class="big" src="%%ASSET_images/femalejoint.png%%" />');
+	                    };
                     };
                 }).fail(function() {
                     return;
